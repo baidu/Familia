5f040ff354755cdeb34d8ff89291a0cbddff6abd
lianrongzhong <lianrongzhong@baidu.com>
2017-06-19 17:10:45 +0800

    modify readme

--- run_show_topic_demo.sh
+++ run_show_topic_demo.sh
@@ -5,4 +5,4 @@ cd model
 sh download_model.sh
 cd ..
 
-./show_topic_demo --vocabulary_path="./model/news/vocab_info.txt" --item_topic_table_path="./model/news/news_slda.model" --top_k=10 --num_topics=2000
+./show_topic_demo --vocabulary_path="./model/news/vocab_info.txt" --item_topic_table_path="./model/news/news_lda.model" --top_k=10 --num_topics=2000
