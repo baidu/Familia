7b805987ecea1fcc82f357c0bff67cede56b34e9
lianrzh <lianrongzhong@Baidu-Macbook-Pro.local>
2017-07-24 19:47:47 +0800

    add webpage topic model

--- model/download_model.sh
+++ model/download_model.sh
@@ -5,3 +5,8 @@ if [ ! -d news ]; then
     wget http://familia.bj.bcebos.com/models/news.v1.tar.gz
     tar -xzf news.v1.tar.gz
 fi
+
+if [ ! -d webpage ]; then
+    wget http://familia.bj.bcebos.com/models/webpage.tar.gz
+    tar -xzf webpage.tar.gz
+fi
